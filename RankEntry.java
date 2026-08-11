ba231b628dbe912b3bed51af08426d0ab3d75269
SilinThoakes <1727253575@qq.com>
2026-08-11 16:52:39 +0000

    清理误解压的摊平文件

--- RankEntry.java
+++
@@ -1,9 +0,0 @@
-package com.dracave.tags.storage;
-
-import java.util.UUID;
-
-/**
- * 排行榜条目：playerId 是查询的 {@code player_uuid}，unlockedCount 是已解锁的称号数，coinBalance 是该玩家的代币余额。
- */
-public record RankEntry(UUID playerId, long unlockedCount, long coinBalance) {
-}
